d66197ea618c94f92e9d728e5d3c6a2dc23f27c2
gabodin <vgabriel2211@gmail.com>
2023-06-15 00:02:49 -0300

    Adição de data retrievement utlizando o banco de dados e QuerySet e feature para coletar receitas associadas a uma categoria

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,5 +7,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home),
     path('home/', views.home, name='home'),
+    path('recipes/category/<int:category_id>',
+         views.recipes_by_category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipe')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from recipes.models import Category, Recipe
 from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+
     return render(request, 'recipes/pages/home.html', status=200, context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def recipes_by_category(request, category_id):
+    recipes_by_category = Recipe.objects.filter(
+        category__id=category_id).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', status=200, context={
+        'recipes': recipes_by_category,
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
+
     return render(request, 'recipes/pages/recipe-view.html', status=200, context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
